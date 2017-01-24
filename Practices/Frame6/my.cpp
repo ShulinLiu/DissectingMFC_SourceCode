8e45a7b96f0261a38db797f4d85c771285c1a0d6
ShirleyL <shulin_liu@foxmail.com>
2017-01-24 09:48:30 +0800

    Frame6

---
+++ Practices/Frame6/my.cpp
@@ -0,0 +1,58 @@
+#include "my.h"
+
+CMyWinApp theApp;
+
+BOOL CMyWinApp::InitInstance()
+{
+    m_pMainWnd = new CMyFrameWnd;
+    return TRUE;
+}
+
+CMyFrameWnd::CMyFrameWnd()
+{
+	std::cout << "CMyFrameWnd Constructor \n";
+    Create();
+}
+
+IMPLEMENT_DYNCREATE(CMyFrameWnd, CFrameWnd)
+IMPLEMENT_DYNCREATE(CMyDoc, CDocument)
+IMPLEMENT_DYNCREATE(CMyView, CView)
+
+void PrintAllClasses()
+{
+    CRuntimeClass* pClass;
+
+    // just walk through the simple list of registered classes
+    for (pClass = CRuntimeClass::pFirstClass; pClass != NULL;
+            pClass = pClass->m_pNextClass)
+    {
+        std::cout << pClass->m_lpszClassName << "\n";
+        std::cout << pClass->m_nObjectSize << "\n";
+        std::cout << pClass->m_wSchema << "\n";
+    }
+}
+//------------------------------------------------------------------
+// main
+//------------------------------------------------------------------
+void main()
+{
+  CWinApp* pApp = AfxGetApp();
+
+  pApp->InitApplication();
+  pApp->InitInstance();
+  pApp->Run();
+
+  //Test Dynamic Creation
+  CRuntimeClass* pClassRef;
+  CObject* pOb;
+  while(1)
+  {
+    if ((pClassRef = CRuntimeClass::Load()) == NULL)
+        break;
+
+    pOb = pClassRef->CreateObject();
+    if (pOb != NULL)
+        pOb->SayHello();
+  }
+}
+//------------------------------------------------------------------
