33ee4a01aa977023e076afebea94871a704e7cd7
ShirleyL <shulin_liu@foxmail.com>
2017-03-01 16:25:21 +0800

    Frame7 Message Map construct and modified some mistakes.

---
+++ Practices/Frame7/afxmsg_.h
@@ -0,0 +1,8 @@
+enum AfxSig
+{
+        AfxSig_end = 0,     // [marks end of message map]
+        AfxSig_vv,
+};
+
+#define ON_COMMAND(id, memberFxn) \
+        { WM_COMMAND, 0, (WORD)id, (WORD)id, AfxSig_vv, (AFX_PMSG)memberFxn },
