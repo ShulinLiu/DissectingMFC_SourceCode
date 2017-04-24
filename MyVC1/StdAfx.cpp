d39ce3b746219a7c0b737f5287d8314a28338e9e
ShirleyL <shulin_liu@foxmail.com>
2017-04-24 20:02:40 +0800

    Delete MYVC1

--- MyVC1/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	MyVC1.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
